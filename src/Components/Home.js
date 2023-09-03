cb39f22dc0af87bd2026e86c9b958259c310f73e
Atul Raj <atulraj0710@gmail.com>
2023-09-03 19:03:18 +0530

    initial game

---
+++ src/Components/Home.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Lottie from  "lottie-react";
+import SpaceBoy from "../LottieFiles/SpaceBoy.json";
+import Typed from "./Typed.js";
+import Tilt from 'react-parallax-tilt';
+import Avatar from '../images/Avatar.png';
+import {CiCoffeeCup} from "react-icons/ci";
+
+const Home = () => {
+  return (
+    <div >
+      <div className='HomePage'>
+
+        <div className='HomeText'>
+          <h1>Hi There!</h1>
+          <h1>I'M <b>ATUL RAJ</b></h1>
+          <Typed/>   
+        </div>
+
+        <Lottie 
+          className="illustration" 
+          animationData={SpaceBoy} 
+          loop={true} 
+        />
+        
+      </div>
+
+      <div className='AboutPage'>
+        <div className='AboutText'>
+          <h1 className='AboutTextHeading'>Brief <b>introduction</b></h1>
+          <p>
+            I love the process of changing raw ideas into a website or a product 
+            that will people and society. 
+            I want to do work that challenges me as a developer & work that I can 
+            be proud of.<br /><br />
+            I am good in problem solving with keen interest in <b>Data Structures and Algorithms</b>.
+            Also, I love to develop web application.<br /><br />
+            I enjoy <b>Coffee</b> and <b>feeding dogs</b>.<CiCoffeeCup style={{scale:"1.5", rotate:"15deg"}}/>   
+          </p>
+        </div>
+        <Tilt>
+          <img className='Avatar' src={Avatar} alt="" />
+        </Tilt>
+      </div>
+    </div>
+  )
+}
+
+export default Home
