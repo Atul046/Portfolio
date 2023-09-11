900f9a8f70bac8898359056fc4314075ed96ab5e
Atul Raj <atulraj0710@gmail.com>
2023-09-11 07:44:31 +0530

    Update ProjectBox.js

--- src/Components/ProjectBox.js
+++ src/Components/ProjectBox.js
@@ -5,17 +5,17 @@ import {CgFileDocument} from "react-icons/cg";
 
 const  ProjectBox = ({projectPhoto, projectName}) => {
   const desc = {
-    TindogDesc : "This website is a landing page of Tinder but for dogs. It is a responsive website which was made to understand Bootstrap. I also learned how to host my project on Github and then how to deploy that project using Github pages.",
-    TindogGithub : "https://github.com/DevanshSahni/tindog",
-    TindogWebsite : "https://devanshsahni.github.io/tindog/",
+    // TindogDesc : "This website is a landing page of Tinder but for dogs. It is a responsive website which was made to understand Bootstrap. I also learned how to host my project on Github and then how to deploy that project using Github pages.",
+    // TindogGithub : "https://github.com/DevanshSahni/tindog",
+    // TindogWebsite : "https://devanshsahni.github.io/tindog/",
 
-    RogFreeDesc : "A website that shows you over seven specialized yoga postures for specific diseases or health problems. This was a group project made in a team of two for a 36-hour-long online hackathon named Hackodisha 2.0.",
-    RogFreeGithub : "https://github.com/DevanshSahni/Rog-Free",
-    RogFreeWebsite : "https://devanshsahni.github.io/Rog-Free/",
+    // RogFreeDesc : "A website that shows you over seven specialized yoga postures for specific diseases or health problems. This was a group project made in a team of two for a 36-hour-long online hackathon named Hackodisha 2.0.",
+    // RogFreeGithub : "https://github.com/DevanshSahni/Rog-Free",
+    // RogFreeWebsite : "https://devanshsahni.github.io/Rog-Free/",
 
-    NewsletterDesc:"A newsletter signup site made using Mailchimp API where the signups can be monitored from the MailChimp account. This project was made to understand API integration, environment variables and vercel deployment.",
-    NewsletterGithub:"",
-    NewsletterWebsite:"https://newsletter-signup-teal.vercel.app/",
+    // NewsletterDesc:"A newsletter signup site made using Mailchimp API where the signups can be monitored from the MailChimp account. This project was made to understand API integration, environment variables and vercel deployment.",
+    // NewsletterGithub:"",
+    // NewsletterWebsite:"https://newsletter-signup-teal.vercel.app/",
 
     IconsMatchingDesc:"It is a simple online fun game which contains various levels. It calculates how much time and how many moves by a player to finish the game.",
     IconsMatchingGithub:"https://github.com/Atul046/Icons_Matching_Game",
